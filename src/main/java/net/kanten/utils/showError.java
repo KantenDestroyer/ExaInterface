fe06e5dad787a1531a3863fddeba95a5d16594c1
martinoleschuk <martinoleschuk@gmail.com>
2024-09-11 15:35:13 +0200

    bug fix

--- src/main/java/net/kanten/utils/showError.java
+++ src/main/java/net/kanten/utils/showError.java
@@ -16,10 +16,9 @@ public class showError {
     public showError(Exception e) {
         StringWriter sw = new StringWriter();
         e.printStackTrace(new PrintWriter(sw));
-        Platform.startup(new Runnable() {
-            @Override
-            public void run() {
-                try {
+
+        try{
+            try {
                 Stage stage = new Stage();
                 FXMLLoader loader = new FXMLLoader(getClass().getResource("/Client/Error.fxml"));
                 Parent root = loader.load();
@@ -31,11 +30,31 @@ public class showError {
                 stage.setScene(scene);
                 stage.setTitle("Error, Please contact Admin");
                 stage.show();
+            }catch(IOException ignored){
+                System.out.println("Error on GUI");
+                System.out.println("If not here error -> " + sw);
+            }
+        }catch(Exception nothing){
+        Platform.startup(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Stage stage = new Stage();
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("/Client/Error.fxml"));
+                    Parent root = loader.load();
+                    errorController eC = loader.getController();
+                    eC.load(sw.toString());
+                    Scene scene = new Scene(root);
+                    scene.getStylesheets().add(String.valueOf(getClass().getResource("/Client/Style.css")));
+                    stage.getIcons().add(new Image(String.valueOf(getClass().getResource("/client/assets/exatrekt-icon.png"))));
+                    stage.setScene(scene);
+                    stage.setTitle("Error, Please contact Admin");
+                    stage.show();
                 }catch(IOException ignored){
                     System.out.println("Error on GUI");
                     System.out.println("If not here error -> " + sw);
                 }
             }
-        });
+        });}
     }
 }
